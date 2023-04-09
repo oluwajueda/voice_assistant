ea707ee5406c4b8692d35fd45d75a58612121c92
oluwajueda <oluwajuedaloade@gmail.com>
2023-04-09 14:35:00 +0100

    add feature box and floating action

--- lib/feature_box.dart
+++ lib/feature_box.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:voiceai/colors.dart';
 
 class FeatureBox extends StatelessWidget {
   final Color color;
-  const FeatureBox({super.key, required this.color});
+  final String headerText;
+  final String description;
+  const FeatureBox(
+      {super.key,
+      required this.color,
+      required this.headerText,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +22,38 @@ class FeatureBox extends StatelessWidget {
       ),
       decoration: BoxDecoration(
           color: color, borderRadius: BorderRadius.all(Radius.circular(15))),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20, left: 15, bottom: 20),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: TextStyle(
+                    fontFamily: "Cera Pro",
+                    color: Pallete.blackColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text(
+                description,
+                style: TextStyle(
+                    fontFamily: "Cera Pro",
+                    color: Pallete.blackColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:voiceai/colors.dart';
+import 'package:voiceai/feature_box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,71 +20,100 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         leading: const Icon(Icons.menu),
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle),
+                  ),
+                ),
+                Container(
+                  height: 123,
                   decoration: const BoxDecoration(
-                      color: Pallete.assistantCircleColor,
-                      shape: BoxShape.circle),
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/virtualAssistant.png"))),
+                )
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              margin: const EdgeInsets.symmetric(
+                horizontal: 40,
+              ).copyWith(top: 30),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius:
+                    BorderRadius.circular(20).copyWith(topLeft: Radius.zero),
+              ),
+              child: const Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  "Good morning, what can I do for you?",
+                  style: TextStyle(
+                    fontFamily: "Cera Pro",
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
+                  ),
                 ),
               ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image:
-                            AssetImage("assets/images/virtualAssistant.png"))),
-              )
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
             ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 40,
-            ).copyWith(top: 30),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor,
-              ),
-              borderRadius:
-                  BorderRadius.circular(20).copyWith(topLeft: Radius.zero),
-            ),
-            child: const Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Text(
-                "Good morning, what can I do for you?",
+            Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: 10, left: 22),
+              child: const Text(
+                "Here are a few features",
                 style: TextStyle(
-                  fontFamily: "Cera Pro",
-                  color: Pallete.mainFontColor,
-                  fontSize: 25,
-                ),
+                    fontFamily: "Cera Pro",
+                    color: Pallete.mainFontColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            margin: EdgeInsets.only(top: 10, left: 22),
-            child: const Text(
-              "Here are a few features",
-              style: TextStyle(
-                  fontFamily: "Cera Pro",
-                  color: Pallete.mainFontColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-          )
-        ],
+            Column(
+              children: [
+                FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: "ChatGPT",
+                  description:
+                      "A smarter way to stay organized and informed with ChatGPT",
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: "Dall-E",
+                  description:
+                      "Get inspired and stay creative with your personal assistant powered by Dall-E",
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: "Smart Voice Assistance",
+                  description:
+                      "Get the best of both worlds with a voice assistance powered by Dall-E and chatGPT",
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: const Icon(Icons.mic),
       ),
     );
   }
